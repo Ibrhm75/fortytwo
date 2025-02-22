3cf92e8288dfeacff1487ef02d2c1799bd6e5ae4
Ibrahim I <itanii060@gmail.com>
2025-02-22 21:43:15 +0200

    added ex00

---
+++ ft_putchar.c
@@ -0,0 +1,14 @@
+// This function displays a character as a parameter
+
+#include<unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main()
+{
+    ft_putchar('c');
+    return 0;
+}
